42015912238e13495c6c02fdc2a10409d77fed34
JaeYeopHan <ljyhanll@gmail.com>
2024-02-25 17:29:26 +0900

    feat: pages directory setup

---
+++ app/pages/home/HomePage.tsx
@@ -0,0 +1,17 @@
+export function HomePage() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <ul>
+        <li>
+          <a href="/">home</a>
+        </li>
+        <li>
+          <a href="/blog">blog</a>
+        </li>
+        <li>
+          <a href="/brand">brand</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
