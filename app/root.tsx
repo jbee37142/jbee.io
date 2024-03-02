3d5a0a128b33824161e9d21647ac0ca075adf72b
JaeYeopHan <ljyhanll@gmail.com>
2024-03-02 17:13:33 +0900

    styles: update favicon

--- app/root.tsx
+++ app/root.tsx
@@ -24,10 +24,14 @@ export const links: LinksFunction = () => [
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="ko">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icon/favicon-16x16.png" />
+        <link rel="manifest" href="/icon/site.webmanifest" />
         <Meta />
         <Links />
       </head>
