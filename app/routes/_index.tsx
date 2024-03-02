be14fbefedbb033be19d6b27121fb6dada27df09
JaeYeopHan <ljyhanll@gmail.com>
2024-03-03 00:25:51 +0900

    styles: support dark theme

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -26,30 +26,24 @@ export default function HomePage() {
     <section className={styles.root}>
       <Logo size={32} />
       <p className={styles.post}>소프트웨어 엔지니어를 지향하며 프런트엔드 개발을 주로 합니다. 기술보다 사람을 좋아하며 타인에게 영향주는 일에 몰입하곤 합니다. 8년째 프런트엔드 개발 컨퍼런스 FEConf를 운영하고 있고 개발 생태계에 관심이 많습니다. 글을 쓰고 공유합니다. 웹과 프런트엔드 주제의 글을 주로 쓰며 가끔 평소 생각을 정리하곤 합니다. 7년째 꾸준히 블로그를 운영하고 있습니다. 수속성입니다. 주 3회 수영을 꾸준히 다닙니다. 스쿠버 다이빙을 하러 분기에 한 번 정도 해외에 나갑니다. 물 속에서의 고요함과 나를 잡아당기던 중력이 사라진 편안함이 좋습니다. 교육에 관심이 많습니다. 교육이 사회에 존재하는 많은 문제를 근본적으로 해결한다고 생각합니다. 어떻게 교육하는지 보단 무엇을 교육해야 하고 어떤 환경을 제공해야 하는지에 더 관심이 많습니다. 지속 가능성을 고민합니다. 설계 뿐만 아니라 프로세스, 시스템, 조직 문화에 대해 고민하곤 합니다. 넓게는 해양 생태계가 지속가능하길 바랍니다. 언젠가 마음 맞는 사람이 모여 영어 유치원을 다닐 필요없는 마을을 만들고 싶습니다.</p>
-      <ul className={styles.rowlist}>
-        <li>
-          <Anchor label="GitHub 링크" href="https://github.com/JaeYeopHan">
-            <SVGIcon.GitHub />
-          </Anchor>
-        </li>
-        <li>
-          <Anchor label="Linkedin 링크" href="https://www.linkedin.com/in/jbee0">
-            <SVGIcon.LinkedIn />
-          </Anchor>
-        </li>
-        <li>
-          <Anchor label="X 링크" href="https://twitter.com/JbeeLjyhanll">
-            <SVGIcon.X />
-          </Anchor>
-        </li>
-      </ul>
-      <section className={styles.root}>
-        <h3 className={styles.h3}>Recent articles</h3>
-        <ul className={styles.list}>
+      <div className={styles.rowlist}>
+        <Anchor label="GitHub 링크" href="https://github.com/JaeYeopHan">
+          <SVGIcon.GitHub />
+        </Anchor>
+        <Anchor label="Linkedin 링크" href="https://www.linkedin.com/in/jbee0">
+          <SVGIcon.LinkedIn />
+        </Anchor>
+        <Anchor label="X 링크" href="https://twitter.com/JbeeLjyhanll">
+          <SVGIcon.X />
+        </Anchor>
+      </div>
+      <section>
+        <h3>Recent articles</h3>
+        <ul>
           {articles.map(({ title, category }, index) => {
             return (
               <li key={index}>
-                <Link className={styles.link} to={`/articles/${category}/${title}`} prefetch="intent">
+                <Link to={`/articles/${category}/${title}`} prefetch="intent">
                   <span>{title}</span>
                 </Link>
               </li>
--- app/routes/index.css.ts
+++ app/routes/index.css.ts
@@ -2,37 +2,20 @@ import { style } from '@vanilla-extract/css';
 import { common } from '~/styles/element.css';
 import { flex, gap } from '~/styles/util.css';
 
-export const root = style([flex('column'), gap(1), {
-  marginTop: '2rem',
-  marginBottom: '1rem',
-}]);
-
-export const h2 = style({
-  fontSize: '1.5rem',
-  fontWeight: 800,
-});
+export const root = style([flex('column'), gap(1)]);
 
 export const post = style({
   fontSize: '1rem',
   lineHeight: 1.7,
   wordWrap: 'break-word',
   wordBreak: 'keep-all',
+  margin: 0,
 });
 
 export const rowlist = style([flex('row'), gap(0.5)]);
-export const list = style([flex('column'), gap(0.5)]);
 
 export const icon = style([common.anchor, {
   display: 'block',
   width: '1.3rem',
   height: '1.3rem',
 }]);
-
-export const link = style([common.anchor, {
-  color: '#4055dc',
-}]);
-
-export const h3 = style({
-  fontSize: '1.3rem',
-  fontWeight: 600,
-});
