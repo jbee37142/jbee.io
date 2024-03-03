287543288cdce90d8e2143fa66301a6b3eac1568
JaeYeopHan <ljyhanll@gmail.com>
2024-03-03 14:43:16 +0900

    infra: change og image to absolute path

--- app/routes/articles_.$category.$article-title.tsx
+++ app/routes/articles_.$category.$article-title.tsx
@@ -3,16 +3,21 @@ import { LoaderFunctionArgs, json, redirect } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
 import { articleQuery } from '~/queries/article';
 import invariant from '~/utils/invariant';
+import { generateMeta } from '~/utils/meta/generate-meta';
 import { safelyFormatDate } from '~/utils/safelyFormatDate';
+import { pathJoin } from '~/utils/path';
 import BlogConfig from '../../blog.config';
 import * as styles from './article.css';
-import { generateMeta } from '~/utils/meta/generate-meta';
 
 export const meta: MetaFunction<typeof loader> = ({ data }) => {
+  const { category, title } = data ?? {};
+  invariant(category != null, '\'category\' is required');
+  invariant(title != null, '\'title\' is required');
+
   return generateMeta({
     title: [data?.title ?? 'Article', BlogConfig.seo.title],
     description: data?.description ?? BlogConfig.seo.description,
-    image: data?.thumbnail,
+    image: pathJoin(BlogConfig.site, 'articles', category, data?.thumbnail ?? ''),
     author: BlogConfig.author.twitter,
     site: BlogConfig.site,
   });
