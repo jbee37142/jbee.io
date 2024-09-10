087ce1d73c617573ac102a18e103a71a37b947fb
Jbee <ljyhanll@gmail.com>
2024-09-10 21:41:02 +0900

    feat: add og route (#3)

---
+++ app/routes/og.tsx
@@ -0,0 +1,35 @@
+import { LoaderFunctionArgs } from '@remix-run/node';
+import { Resvg } from '@resvg/resvg-js';
+import fs from 'fs';
+import satori from 'satori';
+import { OpenGraphTemplate } from '~/components/og/OpenGraphTemplate';
+import { FreesentationFont } from '~/styles/font/freesentation';
+
+export async function loader({
+  request,
+}: LoaderFunctionArgs) {
+  const url = new URL(request.url)
+  const title = url.searchParams.get('title') ?? ''
+  const date = url.searchParams.get('date') ?? ''
+  
+  const svg = await satori(<OpenGraphTemplate title={title} date={date} />, {
+    width: 1280,
+    height: 720,
+    fonts: Object.entries(FreesentationFont).map(([, { filename, weight }]) => {
+      return {
+        name: 'Freesentation',
+        data: fs.readFileSync(`./public/fonts/${filename}`),
+        weight,
+      }
+    }),
+  })
+  const resvg = new Resvg(svg)
+  const pngData = resvg.render()
+  const data = pngData.asPng()
+
+  return new Response(data, {
+    headers: {
+      'Content-Type': 'image/png',
+    },
+  })
+}
