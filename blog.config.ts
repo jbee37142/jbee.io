a5692abee81f093917c966c1c940e204b870db5f
JaeYeopHan <ljyhanll@gmail.com>
2025-03-27 11:40:07 +0900

    fix: change name

--- blog.config.ts
+++ blog.config.ts
@@ -3,7 +3,7 @@ export default {
   site: 'https://jbee.io',
   author: {
     displayName: 'Jbee',
-    twitter: 'jbee_han',
+    twitter: 'jbee37142',
   },
   seo: {
     title: 'Jbee.io',
@@ -17,7 +17,7 @@ export default {
   },
   content: {
     directory: 'content',
-    source: 'https://github.com/JaeYeopHan/jbee.io/edit/main/content/',
+    source: 'https://github.com/jbee37142/jbee.io/edit/main/content/',
   },
   image: {
     main: '/main-image.jpg', // public
@@ -33,9 +33,9 @@ export default {
   내 생각을 세상에 실현시키고 싶어 코딩을 시작했습니다. 웹 프런트엔드 개발을 주로 하며 영역을 넓혀나기는 것을 지향합니다. ⚓️ 문제 해결은 문제를 어떻게 추상화하여 정의하느냐로 결정된다고 생각합니다. 정확한 진단이 있어야 올바른 처방을 할 수 있듯이 문제를 명확히 하는데 투자합니다. 🖊️ 글을 쓰고 공유합니다. 전문적으로 배우지 않았지만 향기로운 글을 쓰기 위해 퇴고합니다. 주로 개인적인 생각을 정리하며 가끔 웹을 소재로 쓰곤 합니다. 누군가에게 도움이 되길 바라며 9년째 블로그를 운영하고 있습니다. 💻 프로그래밍은 제가 가진 문제 해결 도구입니다. 단 코드없이 문제를 해결하려 하고 코드는 최후의 수단이라 여깁니다. 🧭 꾸준함이 가져다 주는 결과를 믿고 실천합니다. 주 3회 수영을 다니며 조금씩 매일 책을 읽습니다. 🎓 교육이 사회에 존재하는 많은 문제를 해결할 수 있다고 생각합니다. 어떻게 교육하는지 보단 무엇을 교육해야 하고 어떤 환경을 제공해야 할지에 더 관심이 많습니다. 다른 모습의 학교가 창조되길 바라며 전혀 다른 형태의 학교가 중심인 마을을 만들고 싶습니다. ⚙️ 지속 가능성을 고민합니다. 소프트웨어 설계 뿐만 아니라 프로세스, 시스템, 문화에 대해 고민하곤 합니다. 개발 생태계에도 관심이 많아 9년째 프런트엔드 개발 컨퍼런스, FEConf를 운영하고 있습니다. 멀리는 해양 생태계가 지속 가능하길 바라며 언젠가 이것에 기여하는 일을 하고 싶습니다.
   `,
   contacts: {
-    github: 'https://github.com/JaeYeopHan',
-    linkedin: 'https://www.linkedin.com/in/jbee0',
-    twitter: 'https://twitter.com/jbee_han',
+    github: 'https://github.com/jbee37142',
+    linkedin: 'https://www.linkedin.com/in/jbee37142',
+    twitter: 'https://twitter.com/jbee37142',
     resume: 'https://docs.google.com/document/d/1NBCuW5DVscjbbaqfj2IkFRhTXzEdPTsPv5zchDXc_-M/edit?usp=sharing',
     instagram: 'https://instagram.com/diver.jbee',
     youtube: 'https://www.youtube.com/@ssoyeop',
